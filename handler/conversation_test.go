fbadeb172b32b8d54354a0ffbb29119b47200b78
Stuart Scott <stuart.murray.scott@gmail.com>
2022-01-07 10:49:18 -0800

    Test Conversation Handler

--- handler/conversation_test.go
+++ handler/conversation_test.go
@@ -23,7 +23,7 @@ func TestConversation(t *testing.T) {
 	assert.Nil(t, err)
 	fs := filesystem.NewOnDisk(dir)
 	defer os.RemoveAll(dir)
-	tmpl, err := template.New("conversation.go.html").Parse(`{{with .Account}}{{.Username}}{{end}}{{.Topic}}{{.Author.Username}}{{.Cost}}{{.Yield}}{{.Content}}`)
+	tmpl, err := template.New("conversation.go.html").Parse(`{{with .Account}}{{.Username}}{{end}}{{.Topic}}{{.Author.Username}}{{.Cost}}{{.Yield}}{{.Content}}{{range .Gifts}}{{.Amount}}{{end}}{{range .Replies}}{{.Cost}}{{end}}`)
 	assert.Nil(t, err)
 	t.Run("Returns 200 When Not Signed In And Conversation Exists", func(t *testing.T) {
 		db := database.NewInMemory()
@@ -37,7 +37,6 @@ func TestConversation(t *testing.T) {
 		assert.Nil(t, err)
 		mime := "text/plain"
 		cost := strconv.FormatInt(size, 10)
-		yield := "0"
 		c, _, _, err := cm.NewConversation(acc, topic, []string{hash}, []string{mime}, []int64{size})
 		assert.Nil(t, err)
 		mux := http.NewServeMux()
@@ -49,7 +48,7 @@ func TestConversation(t *testing.T) {
 		assert.Equal(t, http.StatusOK, result.StatusCode)
 		body, err := io.ReadAll(result.Body)
 		assert.Nil(t, err)
-		assert.Equal(t, topic+authtest.TEST_USERNAME+cost+yield+`<p class="ucc">`+content+`</p>`, string(body))
+		assert.Equal(t, topic+authtest.TEST_USERNAME+cost+"0"+`<p class="ucc">`+content+`</p>`, string(body))
 	})
 	t.Run("Returns 200 When Signed In And Conversation Exists", func(t *testing.T) {
 		db := database.NewInMemory()
@@ -64,7 +63,6 @@ func TestConversation(t *testing.T) {
 		assert.Nil(t, err)
 		mime := "text/plain"
 		cost := strconv.FormatInt(size, 10)
-		yield := "0"
 		c, _, _, err := cm.NewConversation(acc, topic, []string{hash}, []string{mime}, []int64{size})
 		assert.Nil(t, err)
 		mux := http.NewServeMux()
@@ -77,7 +75,7 @@ func TestConversation(t *testing.T) {
 		assert.Equal(t, http.StatusOK, result.StatusCode)
 		body, err := io.ReadAll(result.Body)
 		assert.Nil(t, err)
-		assert.Equal(t, authtest.TEST_USERNAME+topic+authtest.TEST_USERNAME+cost+yield+`<p class="ucc">`+content+`</p>`, string(body))
+		assert.Equal(t, authtest.TEST_USERNAME+topic+authtest.TEST_USERNAME+cost+"0"+`<p class="ucc">`+content+`</p>`, string(body))
 	})
 	t.Run("Returns 404 When Conversation Does Not Exist", func(t *testing.T) {
 		db := database.NewInMemory()
@@ -98,5 +96,64 @@ func TestConversation(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, http.StatusText(http.StatusNotFound)+"\n", string(body))
 	})
-	// TODO Conversation with Reply shows Yield
+	t.Run("Conversation with Reply", func(t *testing.T) {
+		db := database.NewInMemory()
+		ev := authtest.NewEmailVerifier()
+		auth := authgo.NewAuthenticator(db, ev)
+		acc := authtest.NewTestAccount(t, auth)
+		token, _ := authtest.SignIn(t, auth)
+		cm := conveyearthgo.NewContentManager(db, fs)
+		topic := "FooBar"
+		content := "Hello World!"
+		hash, size, err := cm.AddText([]byte(content))
+		assert.Nil(t, err)
+		mime := "text/plain"
+		cost := strconv.FormatInt(size, 10)
+		c, m, _, err := cm.NewConversation(acc, topic, []string{hash}, []string{mime}, []int64{size})
+		assert.Nil(t, err)
+		hash, size, err = cm.AddText([]byte("Hi!"))
+		assert.NoError(t, err)
+		_, _, err = cm.NewMessage(acc, c.ID, m.ID, []string{hash}, []string{mime}, []int64{size})
+		assert.NoError(t, err)
+		mux := http.NewServeMux()
+		handler.AttachConversationHandler(mux, auth, cm, tmpl)
+		request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/conversation?id=%d", c.ID), nil)
+		request.AddCookie(auth.NewSignInSessionCookie(token))
+		response := httptest.NewRecorder()
+		mux.ServeHTTP(response, request)
+		result := response.Result()
+		assert.Equal(t, http.StatusOK, result.StatusCode)
+		body, err := io.ReadAll(result.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, authtest.TEST_USERNAME+topic+authtest.TEST_USERNAME+cost+"1"+`<p class="ucc">`+content+`</p>3`, string(body))
+	})
+	t.Run("Conversation with Gift", func(t *testing.T) {
+		db := database.NewInMemory()
+		ev := authtest.NewEmailVerifier()
+		auth := authgo.NewAuthenticator(db, ev)
+		acc := authtest.NewTestAccount(t, auth)
+		token, _ := authtest.SignIn(t, auth)
+		cm := conveyearthgo.NewContentManager(db, fs)
+		topic := "FooBar"
+		content := "Hello World!"
+		hash, size, err := cm.AddText([]byte(content))
+		assert.Nil(t, err)
+		mime := "text/plain"
+		cost := strconv.FormatInt(size, 10)
+		c, m, _, err := cm.NewConversation(acc, topic, []string{hash}, []string{mime}, []int64{size})
+		assert.Nil(t, err)
+		_, err = cm.NewGift(acc, c.ID, m.ID, 100)
+		assert.NoError(t, err)
+		mux := http.NewServeMux()
+		handler.AttachConversationHandler(mux, auth, cm, tmpl)
+		request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/conversation?id=%d", c.ID), nil)
+		request.AddCookie(auth.NewSignInSessionCookie(token))
+		response := httptest.NewRecorder()
+		mux.ServeHTTP(response, request)
+		result := response.Result()
+		assert.Equal(t, http.StatusOK, result.StatusCode)
+		body, err := io.ReadAll(result.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, authtest.TEST_USERNAME+topic+authtest.TEST_USERNAME+cost+"0"+`<p class="ucc">`+content+`</p>100`, string(body))
+	})
 }
